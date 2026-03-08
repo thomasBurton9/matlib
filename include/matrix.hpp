85190d8cd0de1cc599658593c4052797f7b2b8c9
Thomas Burton <thomasburton9@protonmail.com>
2026-03-08 12:44:59 +1100

    Added printing, adding and indexing data to matrix class

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <cmath>
+#include <ostream>
+#include <print>
+#include <string>
 #include <vector>
+
+
+inline std::string operator*(std::string_view s, std::size_t n) {
+    std::string result{};
+
+    for (std::size_t i = 0; i < n; i++) {
+        result += s;
+    }
+    return result;
+}
 
 class Matrix {
   public:
@@ -8,6 +22,41 @@ class Matrix {
         : x{xSize}, y{ySize}, data(xSize, std::vector<double>(ySize, 0.0)) {}
 
     std::vector<std::vector<double>> data;
+
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &obj) {
+        uint32_t max = 0; // Compare here to keep perf
+        for (std::vector<double> vector : obj.data) {
+            for (double scalar : vector) {
+                if (std::to_string(scalar).size() > max) {
+                    max = std::to_string(scalar).size();
+                }
+            }
+        }
+
+        for (std::vector<double> vector : obj.data) {
+            for (double scalar : vector) {
+                int number_size = std::to_string(scalar).size();
+                std::string full_string = std::string(" ") * static_cast<size_t>(max - number_size + 1);
+                std::print("{}", full_string);
+                std::print("{}", scalar);
+            }
+            std::println("");
+        }
+        return os;
+    };
+
+    Matrix operator+(const Matrix &obj) {
+        Matrix temp(x, y);
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                temp.data[i][j] = data[i][j] + obj.data[i][j];
+            }
+        }
+        return temp;
+    }
+    void operator()(int x, int y) const { std::println("{} ", data[x][y]); }
+
     void display();
     void assignData(std::initializer_list<std::initializer_list<double>> mat);
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <print>
+#include <iostream>
 
 #include "matrix.hpp"
 using std::println;
+using std::cout;
 
 int main() {
     Matrix mat{3, 3};
-    mat.display();
-    mat.assignData({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    mat.display();
+    cout << (mat);
+    mat.assignData({{1000, 2, 3}, {4, 1000, 1}, {7, 8, 6}});
+    Matrix mat2{3, 3};
+    mat2.assignData({{9, 1, 7}, {6, 5, 4}, {3, 2, 1}});
+    cout << (mat);
+    mat(2, 1);
+
+    Matrix tempMat = mat + mat2;
+    cout << tempMat;
 }
