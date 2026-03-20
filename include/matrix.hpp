bbc827b6c41d88c108563fb68b40272b56f42811
Thomas Burton <thomasburton9@protonmail.com>
2026-03-20 19:24:48 +1100

    Added subtraction and division for matrix class, organized code, added shape checking for matmul

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -20,7 +20,7 @@ class Matrix {
 
     std::vector<std::vector<double>> data;
 
-    /// @brief Operator overloading to allow nice printing of Matrix class 
+    /// @brief Operator overloading to allow nice printing of Matrix class
     /// @param os Current output stream
     /// @param obj Matrix to print
     /// @return a stream to print
@@ -67,6 +67,8 @@ class Matrix {
     /// @return A transpose of the current matrix
     Matrix T() { return transpose(); }
 
+    // ADDITION
+
     /// @brief Performs addition of 2 matrices with the same dimensions
     /// @param obj Reference to other matrix
     /// @return A matrix that is the sum of the 2 matrices
@@ -104,6 +106,58 @@ class Matrix {
     friend Matrix operator+(double scalar, const Matrix &obj) {
         return obj + scalar;
     }
+
+    // SUBTRACTION
+
+    /// @brief Performs subtraction of 2 matrices with the same dimensions
+    /// @param obj Reference to other matrix
+    /// @return A matrix that is the difference of the 2 matrices
+    Matrix operator-(const Matrix &obj) const {
+        if (x != obj.x || y != obj.y) {
+            throw std::invalid_argument("Shape mismatch between 2 matrices");
+        }
+        Matrix temp(x, y);
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                temp.data[i][j] = data[i][j] - obj.data[i][j];
+            }
+        }
+        return temp;
+    }
+
+    /// @brief Subtracts a scalar from each value in matrix
+    /// @param scalar The scalar to add
+    /// @return The matrix with subtraction performed on it
+    Matrix operator-(double scalar) const {
+        Matrix temp(x, y);
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                temp.data[i][j] = data[i][j] - scalar;
+            }
+        }
+
+        return temp;
+    }
+    /// @brief Allows scalar addition to be performed when scalar is on the left
+    /// @param scalar The scalar to add
+    /// @param obj A reference to the matrix on the right.
+    /// @return The matrix with addition performed on it
+    friend Matrix operator-(double scalar, const Matrix &obj) {
+        Matrix temp(obj.x, obj.y);
+
+        for (int i = 0; i < obj.x; i++) {
+            for (int j = 0; j < obj.y; j++) {
+                temp.data[i][j] = scalar - obj.data[i][j];
+            }
+        }
+
+        return temp;
+    }
+
+    // MULTIPLICATION
+
     /// @brief Performs scalar multiplication on each value of matrix
     /// @param scalar The scalar to multiply by
     /// @return The matrix that has been multiplied
@@ -121,29 +175,54 @@ class Matrix {
     /// the left
     /// @param scalar The scalar to multiply by
     /// @param obj The matrix to apply multiplication
-    /// @return The matrix that has been multiplied 
+    /// @return The matrix that has been multiplied
     friend Matrix operator*(double scalar, const Matrix &obj) {
         return obj * scalar;
     }
 
-    /// @brief Performs matrix multiplication between 2 matrices with sizes n * m, m * p
+    // Note that there is currently no operation for regular multiplication by item with 2 equally sized n by n matrices.
+    // Potentially move matmul to a regular function and use * operator for itemwise multiplication
+
+    /// @brief Performs matrix multiplication between 2 matrices with sizes n *
+    /// m, m * p
     /// @param obj Other matrix
     /// @return Returns matrix with size n * p
     Matrix operator*(Matrix &obj) const {
         if (y != obj.x) {
-            throw std::invalid_argument("Shape mismatch, inner shapes must be the same");
+            throw std::invalid_argument(
+                "Shape mismatch, inner shapes must be the same");
         }
         // Assert the size of the inner ones is equal
         // Inefficient
         Matrix temp(x, obj.y);
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < obj.y; j++) {
-                temp.data[i][j] = dotProduct(data[i], getColumn(obj.data, j)); // getColumn might be inefficient
+                temp.data[i][j] = dotProduct(
+                    data[i],
+                    getColumn(obj.data, j)); // getColumn might be inefficient
             }
         }
 
         return temp;
     }
+
+    // DIVISION
+
+    /// @brief Performs scalar division on each value of matrix
+    /// @param scalar The scalar to divide by
+    /// @return The matrix that has been divided
+    Matrix operator/(double scalar) const {
+        Matrix temp(x, y);
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                temp.data[i][j] = data[i][j] * scalar;
+            }
+        }
+        return temp;
+    }
+    // Note that there is no division of scalar by matrix given it is generally undefined in linear algebra
+    // Could potentially make it multiply by the inverse of the matrix.
 
     void operator()(int x, int y) const { std::println("{} ", data[x][y]); }
 
@@ -156,3 +235,5 @@ class Matrix {
 };
 
 Matrix identityMatrix(unsigned size);
+
+Matrix gaussianSolve(const Matrix &A, const Matrix &b);
